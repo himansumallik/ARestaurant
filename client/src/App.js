0dec353a1736fdd635f4e8d4d37ec35f139b2ae9
himansu mallick <dreamhimansu@gmail.com>
2023-08-04 11:20:31 +0545

    modified:   client/package-lock.json #	modified:   client/package.json #	modified:   client/src/App.js #	new file:   client/src/components/bodysection/Bodysection.css #	new file:   client/src/components/bodysection/Bodysection.jsx #	new file:   client/src/components/footer/Footer.css #	new file:   client/src/components/footer/Footer.jsx #	new file:   client/src/components/navbar/Navbar.css #	new file:   client/src/components/navbar/Navbar.jsx #	new file:   client/src/containers/MenuItems/MenuItems.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar.jsx'
+import Bodysection from './components/bodysection/Bodysection.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 const App = () => {
   return (
     <div>
-        <h1>Hello world</h1>
+        <Navbar />
+        <Bodysection/>
+        <Footer/>
     </div>
   )
 }
---
+++ client/src/components/bodysection/Bodysection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bodysection = () => {
+  return (
+    <div>
+      <h2>BodySection</h2>
+    </div>
+  )
+}
+
+export default Bodysection
