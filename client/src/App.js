80b0eab0801c2b2c212d2349d37311000f49f1d9
himansu mallick <dreamhimansu@gmail.com>
2023-08-04 12:37:39 +0545

    navbar changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
-import Bodysection from './components/bodysection/Bodysection.jsx'
-import Footer from './components/footer/Footer.jsx'
+// import Bodysection from './components/bodysection/Bodysection.jsx'
+// import Footer from './components/footer/Footer.jsx'
 
 const App = () => {
   return (
     <div>
         <Navbar />
-        <Bodysection/>
-        <Footer/>
     </div>
   )
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {FaBars, FaTimes} from "react-icons/fa";
+import { useRef } from 'react';
+import './Navbar.css'
+
+
 
 const Navbar = () => {
+
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav")
+    }
+
   return (
-    <div>
-      <h2>Navbar</h2>
+    <div id='navbar'>
+      <div><h3>Logo</h3></div>
+      <div>
+        <nav ref={navRef}>
+            <a href="/#"> Home </a>
+            <a href="/#"> Services </a>
+            <a href="/#"> Blog </a>
+            <a href="/#"> About Us </a>
+        </nav>
+      </div>
+      <div>
+        <button className='nav-btn nav-close-btn' onClick={showNavbar} >
+            <FaBars />
+        </button>
+        <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+            <FaTimes />
+        </button>
+      </div>
     </div>
   )
 }
