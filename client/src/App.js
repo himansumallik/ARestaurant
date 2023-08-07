8761a4890cad631db68960043c067c1a626ea109
himansu mallick <dreamhimansu@gmail.com>
2023-08-07 10:59:06 +0545

    #	modified:   client/package-lock.json #	modified:   client/package.json #	modified:   client/src/App.js #	modified:   client/src/components/Footer/Footer.jsx #	modified:   client/src/containers/Home.jsx #	modified:   client/src/images/banner.jpeg

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./containers/Home";
 import About from "./containers/About";
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
     <Layout>
       <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
         <div className="headerContainer">
-          <h1>Food Website</h1>
-          <p>Best Food In India</p>
+          <h1>Food of your choice</h1>
+          <p>Best Indian Foods</p>
           <Link to="/menu">
             <button>ORDER NOW</button>
           </Link>
